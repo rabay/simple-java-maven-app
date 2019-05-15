714725862f7c35fc412962a0e5514e5270abcbd9
Victor Rabay <vrabay81@yahoo.com.br>
2019-05-15 10:32:21 -0300

    Update command for NAME and VERSION

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -11,13 +11,13 @@ set +x
 echo 'The following complex command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME='/var/jenkins_home/apache-maven-3.6.1/bin/mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"'
+NAME=$(/var/jenkins_home/apache-maven-3.6.1/bin/mvn help:evaluate -Dexpression=project.name | grep "^[^\[]")
 set +x
 
 echo 'The following complex command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION='/var/jenkins_home/apache-maven-3.6.1/bin/mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"'
+VERSION=$(/var/jenkins_home/apache-maven-3.6.1/bin/mvn help:evaluate -Dexpression=project.version | grep "^[^\[]")
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
